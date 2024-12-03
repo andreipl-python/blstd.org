c77f589d6f3f151328dfae5887e02087a9a1f4ea
Andrei <257141776a@gmail.com>
2024-12-03 18:11:12 +0300

    refactor: remove unnecessary close buttons from modals
    - Removed 'Close' button from edit booking modal
    - Removed 'Cancel' button from payment modal
    - Kept close icons (x) in modal headers

--- mycrm/booking/views/__init__.py
+++ mycrm/booking/views/__init__.py
@@ -2,4 +2,3 @@ from .user_index import user_index_view
 from .menu2 import menu2_view
 from .create_booking import create_booking_view
 from .edit_booking import *
-from .payment_booking import payment_booking_view
